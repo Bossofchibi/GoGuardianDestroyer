a3bbf5c9fdb9038ff88f1d1dd56c13837ee787a5
@OfficialCharlieChaplin <124716301+Bossofchibi@users.noreply.github.com>
2023-06-08 06:58:45 -0400

    Update Killer.js

--- Killer.js
+++ Killer.js
@@ -1,13 +1,31 @@
 function removeExtension(filename) {
-    
     let parts = filename.split('.');
-    
     parts.pop();
-    
     return parts.join('.');
 }
 
+let filenames = [
+  "haldlgldplgnggkjaafhelgiaglafanh",
+  "ihjgnoifhnilgbjicdpingfgjhjeffij",
+  "kmpjlilnemjciohjckjadmgmicoldglf",
+  "ifeifkfohlobcbhmlfkenopaimbmnahb",
+  "jaoebcikabjppaclpgbodmmnfjihdngk",
+  "kbohafcopfpigkjdimdcdgenlhkmhbnc",
+  "gcjpefhffmcgplgklffgbebganmhffje",
+  "lgcbihdlknkcmmnapfocjbkdefkhmolo"
+];
 
-let filename = ["haldlgldplgnggkjaafhelgiaglafanh", "ihjgnoifhnilgbjicdpingfgjhjeffij", "kmpjlilnemjciohjckjadmgmicoldglf", "ifeifkfohlobcbhmlfkenopaimbmnahb", "jaoebcikabjppaclpgbodmmnfjihdngk", "kbohafcopfpigkjdimdcdgenlhkmhbnc", "gcjpefhffmcgplgklffgbebganmhffje", "lgcbihdlknkcmmnapfocjbkdefkhmolo"]
-let newFilename = removeExtension(filename);
-console.log(newFilename); 
+let newFilenames = [];
+for (let i = 0; i < filenames.length; i++) {
+  let newFilename = removeExtension(filenames[i]);
+  newFilenames.push(newFilename);
+}
+
+console.log(newFilenames);
+
+let newFilenames = [];
+filenames.forEach(function(filename) {
+  let newFilename = removeExtension(filename);
+  newFilenames.push(newFilename);
+});
+
